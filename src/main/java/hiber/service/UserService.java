51c7a675ce52115a0d811cb3b8b17b33c6a67b10
bermik <102170118+bermik@users.noreply.github.com>
2022-05-06 23:19:06 +0900

    migrated to spring Boot

--- src/main/java/hiber/service/UserService.java
+++ src/main/java/hiber/service/UserService.java
@@ -1,13 +1,35 @@
 package hiber.service;
 
 import hiber.model.User;
+import hiber.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-public interface UserService {
-    List<User> allUsers();
-    void add(User user);
-    void delete(Long id);
-    void edit(User user);
-    User find(Long id);
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public List<User> allUsers() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public void add(User user) {
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public User find(Long id) { return userRepository.getById(id); }
 }
